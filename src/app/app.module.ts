f9dcf33dbde7c90502ae00c9b9e540175755409d
Pedro Baracho <pedropbaracho@gmail.com>
2017-01-15 21:23:23 -0200

    Finished exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormAppComponent } from './form-app/form-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormAppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
